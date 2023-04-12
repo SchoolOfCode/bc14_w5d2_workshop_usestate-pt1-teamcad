bb3d952e372e251743b0be316ac6117f9de23653
DeeqaJamalini <deeqa.j11@gmail.com>
2023-04-12 12:14:39 +0100

    Task 1 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
 import "./App.css";
+import { useState } from "react";
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleCounterClick()
+  {
+    setCount(count + 1);
+  }
   return (
     <main className="App">
       <h1>React useState</h1>
       <section>
-        <h2 className="count"></h2>
-        <button className="countButton">+1</button>
+        <h2 className= "count"> {count}</h2>
+        <button className="countButton" onClick={handleCounterClick
+        }>+1</button>
       </section>
       <section>
         <p className="randomBootcamper">BOOTCAMPER NAME</p>
